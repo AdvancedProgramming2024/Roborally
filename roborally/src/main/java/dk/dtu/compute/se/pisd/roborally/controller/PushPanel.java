322ef6425223872bd2a836ea825d28aa2d0b8ed2
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2024-04-24 22:29:36 +0200

    Minor fix to push panels
    Step starts from 0, så it pushed on the wrong steps

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PushPanel.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PushPanel.java
@@ -22,9 +22,9 @@ public class PushPanel extends FieldAction {
     public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
         Player player = space.getPlayer();
         if (player == null) return false;
-        if (pushTime == PushTime.EVEN && space.board.getStep() % 2 == 0) {
+        if (pushTime == PushTime.EVEN && space.board.getStep() % 2 + 1 == 0) {  // +1 because step is 0-indexed
             gameController.moveInDirection(player, heading);
-        } else if (pushTime == PushTime.ODD && space.board.getStep() % 2 == 1) {
+        } else if (pushTime == PushTime.ODD && space.board.getStep() % 2 + 1 == 1) {  // +1 because step is 0-indexed
             gameController.moveInDirection(player, heading);
         }
         return true;
