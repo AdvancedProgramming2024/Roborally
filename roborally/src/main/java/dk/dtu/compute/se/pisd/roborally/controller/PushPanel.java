52810933a8c73d8f0ae444c6625ea518283f17ce
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2024-04-25 16:22:21 +0200

    Slight fix for push panel

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PushPanel.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PushPanel.java
@@ -22,9 +22,9 @@ public class PushPanel extends FieldAction {
     public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
         Player player = space.getPlayer();
         if (player == null) return false;
-        if (pushTime == PushTime.EVEN && space.board.getStep() % 2 + 1 == 0) {  // +1 because step is 0-indexed
+        if (pushTime == PushTime.EVEN && (space.board.getStep() + 1) % 2 == 0) {  // +1 because step is 0-indexed
             gameController.moveInDirection(player, heading);
-        } else if (pushTime == PushTime.ODD && space.board.getStep() % 2 + 1 == 1) {  // +1 because step is 0-indexed
+        } else if (pushTime == PushTime.ODD && (space.board.getStep() + 1) % 2 == 1) {  // +1 because step is 0-indexed
             gameController.moveInDirection(player, heading);
         }
         return true;
