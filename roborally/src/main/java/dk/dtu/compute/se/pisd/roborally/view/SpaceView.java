5fdaab76773dd5871a399d8fbef16ef6195ea422
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2024-05-03 13:52:13 +0200

    Author and param tags

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
@@ -170,7 +170,10 @@ public class SpaceView extends StackPane implements ViewObserver {
             BoardView.getSpaceView(hit).getChildren().add(laserImageView4);
         }
     }
-
+    /**
+     * Sets all laser images to null and clears the list of lasers
+     * @author Kresten (s235103)
+     */
     public static void destroyLasers() {
         for (ImageView laser : lasers) {
             laser.setImage(null);
@@ -307,6 +310,11 @@ public class SpaceView extends StackPane implements ViewObserver {
         }
     }
 
+    /**
+     * @author Kresten (s235103)
+     * @param spaceImageView ImageView of the space
+     * @param gear Gear to be drawn
+     */
     private void drawGear(ImageView spaceImageView, Gear gear) {
         Image spaceImage;
         if (gear.getHeading() == Heading.WEST) {
@@ -317,6 +325,12 @@ public class SpaceView extends StackPane implements ViewObserver {
         spaceImageView.setImage(spaceImage);
     }
 
+    /**
+     * Draws the energy field on the board. Initially draws an energyCube
+     * @author Kresten (s235103)
+     * @param spaceImageView ImageView of the space
+     * @param energyCubeField EnergyCubeField to be drawn
+     */
     private void drawEnergyField(ImageView spaceImageView, EnergyCubeField energyCubeField) {
         Image spaceImage = new Image("images/energyField.png");
         spaceImageView.setImage(spaceImage);
@@ -333,6 +347,12 @@ public class SpaceView extends StackPane implements ViewObserver {
         this.getChildren().add(energyCubeImageView);
     }
 
+    /**
+     * Draws the checkpoint on the board
+     * @author Kresten (s235103)
+     * @param spaceImageView ImageView of the space
+     * @param checkpoint Checkpoint to be drawn
+     */
     private void drawCheckpoint(ImageView spaceImageView, Checkpoint checkpoint) {
         Image spaceImage = switch (checkpoint.getId()) {
             case 2 -> new Image("images/checkpoint2.png");
@@ -345,6 +365,11 @@ public class SpaceView extends StackPane implements ViewObserver {
         spaceImageView.setImage(spaceImage);
     }
 
+    /**
+     * Draws the conveyor belt on the board
+     * @author Kresten (s235103)
+     * @param spaceImageView ImageView of the space
+     */
     private void drawConveyorBelt(ImageView spaceImageView) {
         Image spaceImage = new Image("images/greenConveyor.png");
         spaceImageView.setImage(spaceImage);
@@ -364,6 +389,11 @@ public class SpaceView extends StackPane implements ViewObserver {
         }
     }
 
+    /**
+     * Creates an ImageView of the laser to be drawn on the board
+     * @author Kresten (s235103)
+     * @return ImageView of the laser
+     */
     @NotNull
     private ImageView createLaserImageView() {
         ImageView laserImageView = new ImageView();
@@ -405,6 +435,10 @@ public class SpaceView extends StackPane implements ViewObserver {
         return laserImageView;
     }
 
+    /**
+     * @author Kresten (s235103)
+     * @return Laser if the space contains one, null otherwise
+     */
     private Laser getLaser() {
         if (space.getActions().isEmpty()) {
             return null;
